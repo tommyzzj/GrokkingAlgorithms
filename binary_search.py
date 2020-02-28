05e36bb7ae99557ecc7c8d53836abac93399a751
Tommy Zou <tommyzzj@hotmail.com>
2020-02-28 23:12:23 +0800

    Fix the binary search

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,19 @@
-print("hello, world!")
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = round((low + high)/2)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid
+        else:
+            low = mid
+    return None
+
+
+my_list = [1, 3, 5, 7, 9, 11, 12]
+
+print(binary_search(my_list, 12))
